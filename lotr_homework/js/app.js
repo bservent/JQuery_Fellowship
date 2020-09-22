56ded588c0aab8124b4a546692579f3e72d9e511
Brittany Servent <brittybear@Brittanys-MBP.domain>
2020-09-21 19:46:43 -0700

    Chapter 2 complete - Made the Hobbits

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -1,6 +1,6 @@
-console.log("LOTR homework js file is connected");
+/* console.log("LOTR homework js file is connected");
 console.log("HINT: Click on the 'Elements' tab to see the elements that you are creating");
-console.log("---------------")
+console.log("---------------") */
 // ==============================
 //       Dramatis Personae
 // ==============================
@@ -47,11 +47,17 @@ const makeMiddleEarth = () => {
   console.log("Trying to make middle earth.");
 
   // 1. create a section tag with an id of middle-earth
-
+     const middleEarth = $(document.createElement('section'));
+     middleEarth.attr('id', 'middle-earth');
   // 2. append the section to the body of the DOM.
-
+      $('body').append(middleEarth);
   // 3. use a for loop to iterate over the lands array that does the following:
-
+    for (let i = 0; i < lands.length; i ++) {
+      const land = $('<article />')
+      land.attr('id', lands[i]);
+      land.append($('<h1>' + lands[i] + '</h1>')) 
+      middleEarth.append(land)
+    }
   //   3a. creates an article tag (there should be one for each land when the loop is done)
 
   //   3b. gives each land article an `id` tag of the corresponding land name
